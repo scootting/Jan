402d885c6f89f0c5409c70828b900fe36554c030
scootting <viadhose@hotmail.com>
2022-10-03 20:41:35 -0400

    hola

--- resources/js/views/components/Person.vue
+++ resources/js/views/components/Person.vue
@@ -10,22 +10,28 @@
         >It should be noted that the content will not be aligned in center by
         default</span
       >
+      <el-input placeholder="Please input" v-model="message"></el-input>
       <span slot="footer" class="dialog-footer">
         <el-button @click="closeModal">Cancel</el-button>
-        <el-button type="primary" @click="closeModal" @close="cierren">Confirm</el-button>
+        <el-button type="primary" @click="closeModal">Confirm</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 <script>
 export default {
-  name: "Personed",
+  name: "Person",
   props: {
     centerDialogVisible: {
       required: true,
       type: Boolean,
       default: false,
     },
+    message:{
+      required: true,
+      type: Text,
+      default: '',
+    }
   },
   data() {
     return {
@@ -34,9 +40,6 @@ export default {
   },
   computed: {},
   methods: {
-    cierren(){
-      console.log("pruebas de como se cierra");
-    },
     closeModal() {
       this.isCenterDialogVisible = false;
       this.$emit("update-visible", this.isCenterDialogVisible);
--- resources/js/views/singlebudget/SingleBudgetDetail.vue
+++ resources/js/views/singlebudget/SingleBudgetDetail.vue
@@ -1,319 +1,163 @@
 <template>
-    <div>
-      <el-card class="box-card">
-        <div slot="header" class="clearfix">
-          <span>detalle del archivo</span>
-          <el-button
-            style="float: right; padding: 3px 0"
-            type="text"
-            @click="test"
-            >ayuda</el-button
-          >
-        </div>
-        <el-row :gutter="50">
-          <!--
-          <el-col :span="24"
-            ><div class="grid-content bg-purple">
-              <el-form
-                ref="form"
-                :model="this.personal"
-                label-width="200px"
-                size="mini"
-              >
-                <el-form-item label="carnet de identidad">
-                  <el-input v-model="personal.nro_dip" disabled></el-input>
-                </el-form-item>
-                <el-form-item label="apellido paterno">
-                  <el-input v-model="personal.paterno" disabled></el-input>
-                </el-form-item>
-                <el-form-item label="apellido materno">
-                  <el-input v-model="personal.materno" disabled></el-input>
-                </el-form-item>
-                <el-form-item label="nombres">
-                  <el-input v-model="personal.nombres" disabled></el-input>
-                </el-form-item>
-              </el-form>
-            </div></el-col
-          >
-          -->
-          <el-col :span="24"
-            ><div class="grid-content bg-purple">
-              <el-table
-                :data="documentsArchive"
-                border
-                style="width: 100%"
-                size="small"
-              >
-                <el-table-column
-                  prop="indice"
-                  label="indice"
-                  align="right"
-                  width="50"
-                >
-                </el-table-column>
-                <el-table-column
-                  prop="fecha"
-                  label="fecha"
-                  width="100"
-                  align="center"
-                >
-                </el-table-column>
-                <el-table-column
-                  prop="numeral"
-                  label="no. documento"
-                  width="100"
-                  align="right"
-                >
-                </el-table-column>
-                <el-table-column prop="descr" label="documento" width="250" align="center">
-                  <template slot-scope="scope">
-                    <el-tag size="medium">{{
-                      scope.row.descr
-                    }}</el-tag>
-                  </template>
-                </el-table-column>
-                <el-table-column prop="glosa" label="descripcion" width="750">
-                </el-table-column>
-                <el-table-column
-                  align="right-center"
-                  label="operaciones"
-                  width="180"
-                >
-                  <template slot-scope="scope">
-                    <el-button
-                      :disabled="scope.row.guardado === true"
-                      type="text"
-                      size="mini"
-                      @click="initEditDocumentOfArchive(scope.$index, scope.row)"
-                      >editar</el-button
-                    >
-                    <el-button
-                      :disabled="scope.row.guardado === true"
-                      type="text"
-                      size="mini"
-                      @click="DeleteDocumentOfArchive(scope.$index, scope.row)"
-                      >quitar</el-button
-                    >
-                  </template>
-                </el-table-column>
-              </el-table>
-            </div></el-col
-          >
-        </el-row>
-        <!-- Form Add Document to Archive-->
-        <el-button type="text" @click="initAddDocumentOfArchive"
-          >Agregar nuevo documento</el-button
-        >
-        <el-dialog
-          title="detalle del documento"
-          :visible.sync="dialogFormVisible"
-        >
-          <el-form :model="document" label-width="220px" size="small">
-            <el-form-item label="tipo de documento">
-              <el-select
-                v-model="document.id_tipo"
-                value-key="descr"
-                size="small"
-                placeholder="seleccione el tipo de documento" @change="OnchangeTypeDocument"
-              >
-                <el-option
-                  v-for="item in typesDocument"
-                  :key="item.id"
-                  :label="item.descr"
-                  :value="item.id"
-                >
-                </el-option>
-              </el-select>
-            </el-form-item>
-            <el-form-item label="Numero del documento">
-              <el-input v-model="document.numeral" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="fecha del registro">
-              <el-date-picker
-                type="date"
-                v-model="document.fecha"
-                placeholder="seleccione una fecha"
-                style="width: 100%"
-                format="yyyy/MM/dd"
-                value-format="yyyy-MM-dd"
-              ></el-date-picker>
-            </el-form-item>
+  <div>
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>detalle del presupuesto individual</span>
+        <el-button style="float: right; padding: 3px 0" type="text" @click="test">ayuda</el-button>
+      </div>
+      <!-- Form Add Container to Archive-->
+      <!--
+      <el-dialog title="detalle del documento" :visible.sync="dialogFormVisible">
+        <el-form :model="document" label-width="220px" size="small">
+          <el-form-item label="tipo de documento">
+            <el-select v-model="document.id_tipo" value-key="descr" size="small"
+              placeholder="seleccione el tipo de documento" @change="OnchangeTypeDocument">
+              <el-option v-for="item in typesDocument" :key="item.id" :label="item.descr" :value="item.id">
+              </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Numero del documento">
+            <el-input v-model="document.numeral" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="fecha del registro">
+            <el-date-picker type="date" v-model="document.fecha" placeholder="seleccione una fecha" style="width: 100%"
+              format="yyyy/MM/dd" value-format="yyyy-MM-dd"></el-date-picker>
+          </el-form-item>
+
+          <el-form-item label="glosa o descripcion">
+            <el-input type="textarea" v-model="document.glosa" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button type="primary" size="small" plain @click="AddDocumetOfArchive">Confirmar</el-button>
+          <el-button type="danger" size="small" plain @click="dialogFormVisible = false">Cancelar</el-button>
+        </span>
+      </el-dialog>
+      -->
+      <!-- Form Add Document to Archive-->
+      <!-- Form Add Container to Archive-->
+      <!-- Form Add Document to Archive-->
+      <el-button type="text" @click="initSearchPerson">Agregar persona</el-button>
+      <persona :centerDialogVisible="isVisible" @update-visible="update"></persona>
+    </el-card>
+  </div>
+</template>
   
-            <el-form-item label="glosa o descripcion">
-              <el-input
-                type="textarea"
-                v-model="document.glosa"
-                autocomplete="off"
-              ></el-input>
-            </el-form-item>
-            <!--
-          -->
-          </el-form>
-          <span slot="footer" class="dialog-footer">
-            <el-button
-              type="primary"
-              size="small"
-              plain
-              @click="AddDocumetOfArchive"
-              >Confirmar</el-button
-            >
-            <el-button
-              type="danger"
-              size="small"
-              plain
-              @click="dialogFormVisible = false"
-              >Cancelar</el-button
-            >
-          </span>
-        </el-dialog>
-        <!-- Form Add Document to Archive-->
-        <!-- Form Add Container to Archive-->
-        <el-button type="text" @click="initAddArchiveOfContainer"
-          >Agregar contenedor</el-button
-        >
-        <!-- Form Add Container to Archive-->
-      </el-card>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "",
-    data() {
-      return {
-        user: this.$store.state.user,
-        id_archive: null,
-        documentsArchive: [],
-        typesDocument: [],
-        dialogFormVisible: false,
-        stateStore: "",
-        document: {
-          id_doc: "",
-          numeral: "",
-          indice: "",
-          glosa: "",
-          fecha: "",
-          id_tipo: null,
-          descr: "",
-          gestion: null,
-        },
-      };
+<script>
+import persona from "../components/Person.vue";
+export default {
+  name: "",
+  components: {
+    persona,
+  },
+  data() {
+    return {
+      user: this.$store.state.user,
+      documentsArchive: [],
+      isVisible: false,
+
+      stateStore: "",
+      document: {
+        id_doc: "",
+        numeral: "",
+        indice: "",
+        glosa: "",
+        fecha: "",
+        id_tipo: null,
+        descr: "",
+        gestion: null,
+      },
+    };
+  },
+  mounted() {
+    let app = this;
+    app.id = app.$route.params.id;
+    //app.getTypesDocument();
+    //app.getDocumentsbyArchive();
+  },
+  methods: {
+
+    //  * Todas las funciones para el componente persona
+    initSearchPerson() {
+      this.isVisible = true;
     },
-    mounted() {
+    update(isVisible) {
+      this.isVisible = isVisible;
+    },
+
+    test(){
+      console.log("Ayuda!!!");
+    },
+    //  * A3. Obtiene la lista de tipos de documentos que pertenecen a un archivo
+    async getTypesDocument() {
       let app = this;
-      app.id_archive = app.$route.params.id;
-      app.getTypesDocument();
-      app.getDocumentsbyArchive();
-    },
-    methods: {
-      test() {
-        alert("test");
-      },
-      //  * A3. Obtiene la lista de tipos de documentos que pertenecen a un archivo
-      async getTypesDocument() {
-        let app = this;
-        try {
-          let response = await axios.post("/api/getTypesDocument", {
-            description: "Documento",
-          });
-          app.typesDocument = response.data;
-          console.log(app.typesDocument);
-          console.log(app.documentsArchive);
-        } catch (error) {
-          console.log(error);
-        }
-      },
-      //  * A2. Obtiene la lista de documentos que pertenecen a un archivo
-      async getDocumentsbyArchive() {
-        let app = this;
-        try {
-          let response = await axios.post("/api/getDocumentsbyArchive", {
-            id: app.id_archive,
-            year: app.user.gestion,
-          });
-          app.documentsArchive = response.data;
-          console.log(app.documentsArchive);
-        } catch (error) {
-          this.error = error.response.data;
-          app.$alert(this.error.message, "Gestor de errores", {
-            dangerouslyUseHTMLString: true,
-          });
-        }
-      },
-      //  * Inicia la edicion de un documento
-      initEditDocumentOfArchive(idx, row) {
-        this.document = row;
-        this.stateStore = "editar";
-        console.log(this.document);
-        this.dialogFormVisible = true;
-      },
-      //  * Inicia un nuevo documento
-      initAddDocumentOfArchive() {
-        this.document = {
-          id_doc: "",
-          numeral: "",
-          indice: "",
-          glosa: "",
-          fecha: "",
-          id_tipo: null,
-          descr: "",
-          gestion: null,
-        };
-        this.stateStore = "añadir";
-        this.dialogFormVisible = true;
-      },
-  
-      initAddArchiveOfContainer(idx, row){
-        alert('hola como estas?');
-      },
-      //  * Guarda los cambios de un nuevo documento sea nuevo o uno ya existente
-      AddDocumetOfArchive() {
-        if (this.stateStore == "añadir")
-          this.documentsArchive.push(this.document);
-        this.dialogFormVisible = false;
-        console.log(this.documentsArchive);
-      },
-  
-      //  * Quita un documento ya existente
-      DeleteDocumentOfArchive(idx, row) {
-        this.documentsArchive.splice(idx, 1);
-        console.log(this.documentsArchive);
-      },
-      OnchangeTypeDocument(idx){
-          this.document.descr = this.typesDocument[idx-1].descr;
+      try {
+        let response = await axios.post("/api/getTypesDocument", {
+          description: "Documento",
+        });
+        app.typesDocument = response.data;
+        console.log(app.typesDocument);
+        console.log(app.documentsArchive);
+      } catch (error) {
+        console.log(error);
       }
     },
-  };
-  </script>
+    //  * A2. Obtiene la lista de documentos que pertenecen a un archivo
+    async getDocumentsbyArchive() {
+      let app = this;
+      try {
+        let response = await axios.post("/api/getDocumentsbyArchive", {
+          id: app.id_archive,
+          year: app.user.gestion,
+        });
+        app.documentsArchive = response.data;
+        console.log(app.documentsArchive);
+      } catch (error) {
+        this.error = error.response.data;
+        app.$alert(this.error.message, "Gestor de errores", {
+          dangerouslyUseHTMLString: true,
+        });
+      }
+    },
+
+  },
+};
+</script>
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  .el-row {
-    margin-bottom: 20px;
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    padding: 15px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
-  
-  .el-form .el-select {
-    width: 100%;
-  }
-  </style>
+<style scoped>
+.el-row {
+  margin-bottom: 20px;
+}
+
+.el-col {
+  border-radius: 4px;
+}
+
+.bg-purple-dark {
+  background: #99a9bf;
+}
+
+.bg-purple {
+  background: #d3dce6;
+}
+
+.bg-purple-light {
+  background: #e5e9f2;
+}
+
+.grid-content {
+  border-radius: 4px;
+  padding: 15px;
+  min-height: 36px;
+}
+
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+
+.el-form .el-select {
+  width: 100%;
+}
+</style>
